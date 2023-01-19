6d62a508d1d50644bcfe872a3251c58ca737745f
TIJOU Allan <allan.tijou8@etu.univ-lorraine.fr>
2023-01-19 16:16:34 +0100

    changement nom + premiere fonction init

---
+++ src/controleur/class_abonnement_liste.ts
@@ -0,0 +1,32 @@
+import {LesAbonnements} from "../modele/data_abonnement"
+ type TAbonnementListeForm = {
+    /* page abonnement_liste.html */
+ tableAbonnement : HTMLTableElement,
+ btnDetail :HTMLInputElement, btnAjouter :HTMLInputElement, 
+btnModifier :HTMLInputElement, btnSupprimer : HTMLInputElement,
+
+
+}
+class VueAbonnementListe {
+   private _grille : GrilleTabulaire;
+   private _data : TdataSet;
+   private _form : TAbonnementListeForm
+   constructor() {/*rien */ }
+
+   get form() :TAbonnementListeForm {return this._form}
+   get data() :TdataSet {return this._data}
+   get grille() :GrilleTabulaire {return this._grille}
+
+init(form : TAbonnementListeForm):void{
+   this._grille = new GrilleTabulaire;
+   this._data = [];
+   this._form = form;
+   
+   const lesAbonnements = new LesAbonnements;
+   this._data = lesAbonnements.listAll();
+
+   this._grille = APIpageWeb.showArray(this.form.tableAbonnement.id,this._data,'abonNum',true)
+}
+}
+
+
